bf01905aed9945af650d5314f9dec82384dddde5
Samujjal Majumder <samujjal.majumder@traveloka.com>
2019-07-20 15:20:43 +0530

    First Solution completed

--- src/main/java/dev/samujjal/coding/interview/SigtupleRunner.java
+++ src/main/java/dev/samujjal/coding/interview/SigtupleRunner.java
@@ -1,5 +1,6 @@
 package dev.samujjal.coding.interview;
 
+import dev.samujjal.coding.interview.service.Solution1Service;
 import dev.samujjal.coding.interview.service.Solution2Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,8 +10,12 @@ import org.springframework.stereotype.Service;
 public class SigtupleRunner implements CommandLineRunner {
   @Autowired
   private Solution2Service solution2Service;
+
+  @Autowired
+  private Solution1Service solution1Service;
   @Override
   public void run(String... args) throws Exception {
+    solution1Service.run();
     solution2Service.run();
   }
 }
--- src/main/java/dev/samujjal/coding/interview/service/Solution1Service.java
+++ src/main/java/dev/samujjal/coding/interview/service/Solution1Service.java
@@ -1,8 +1,10 @@
 package dev.samujjal.coding.interview.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class Solution1Service {
 
     public int[] getBuildingInput(){
@@ -10,6 +12,25 @@ public class Solution1Service {
     }
 
     public void run(){
-        
+        int[] buildingStats = getBuildingInput();
+        int[] leftHighest = new int[buildingStats.length];
+        leftHighest[0] = buildingStats[0];
+        int[] rightHighest = new int[buildingStats.length];
+        rightHighest[rightHighest.length-1] = buildingStats[buildingStats.length - 1];
+
+        for (int i = 1; i < buildingStats.length; i++) {
+            leftHighest[i] = Math.max(leftHighest[i-1], buildingStats[i]);
+        }
+
+        for (int i = rightHighest.length-2; i >= 0 ; i--) {
+            rightHighest[i] = Math.max(rightHighest[i+1], buildingStats[i]);
+        }
+
+        int totalVolumne = 0;
+        for (int i = 0; i < buildingStats.length; i++) {
+            totalVolumne += Math.min(leftHighest[i], rightHighest[i]) - buildingStats[i];
+        }
+
+        log.info("Total possible rainwater harvested : {}", totalVolumne);
     }
 }
